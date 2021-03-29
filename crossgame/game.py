15baf547a95bc4c510e04942af9f2e9cc0e51c68
Yushua <yushabakker1@gmail.com>
2021-03-29 10:43:55 +0200

    pushing

--- crossgame/game.py
+++ crossgame/game.py
@@ -1,6 +1,7 @@
 import pygame
 from gameObjects import GameObjects
 from player import Player
+from enemy import Enemy
 
 class Game:
     def __init__(self):
@@ -15,6 +16,7 @@ class Game:
         self.backgroundpng = 'crossgame/assets/background.png'
         self.character_frontpng = 'crossgame/assets/character_front1.png'
         self.chestpng = 'crossgame/assets/chest.png'
+        self.keypng = 'crossgame/assets/key.png'
         #endif
 
         self.clock = pygame.time.Clock()
@@ -22,15 +24,54 @@ class Game:
         #set objects
         self.background = GameObjects(0, 0, self.width, self.height, self.backgroundpng, 0)
         self.chest = GameObjects(360, 48, self.block, self.block, self.chestpng, 0)
-        self.player = Player(336, 672, self.block, self.block, self.character_frontpng, 0, 5)
+        self.level = 1.0
+        self.set_map()
         #endif
 
+    def set_map(self):
+        self.player = Player(336, 672, self.block, self.block, self.character_frontpng, 0, 4)
+        self.chestpng = 'crossgame/assets/chest.png'
+        self.chest = GameObjects(360, 48, self.block, self.block, self.chestpng, 0)
+        self.enemies = [
+            Enemy(0, 144, self.block, self.block, self.keypng, 1, 2),
+            Enemy(0, 144 + self.block, self.block, self.block, self.keypng, 1, 2),
+            Enemy(0, 144 - self.block, self.block, self.block, self.keypng, 1, 2),
+        ]
+
+    def move_objects(self, player_direction_y, player_direction_x):
+        self.player.move(player_direction_y, player_direction_x, self.height, self.width)
+        for enemy in self.enemies:
+            enemy.move(self.width)
+
+    def check_if_collided(self):
+        for enemy in self.enemies:
+            if self.detect_collision(self.player, enemy):
+                self.set_map()
+                return True
+        if self.detect_collision(self.player, self.chest):
+            self.chestpng = 'crossgame/assets/chest_open.png'
+            self.chest = GameObjects(self.chest.x, self.chest.y, self.chest.width, self.chest.height, self.chestpng, 0)
+            self.game_window.blit(self.chest.image, (self.chest.x, self.chest.y))
+        return False
+
+    def detect_collision(self, object_1, object_2):
+        if  object_1.y > (object_2.y + object_2.height):
+            return False
+        elif (object_1.y + object_1.height) < object_2.y:
+            return False
+        if  object_1.x > (object_2.x + object_2.width):
+            return False
+        elif (object_1.x + object_1.width) < object_2.x:
+            return False
+        return True
 
     def draw_objects(self):
         self.game_window.fill(self.colour)
         self.game_window.blit(self.background.image, (0, 0))
         self.game_window.blit(self.chest.image, (self.chest.x, self.chest.y))
         self.game_window.blit(self.player.image, (self.player.x, self.player.y))
+        for enemy in self.enemies:
+            self.game_window.blit(enemy.image, (enemy.x, enemy.y))
         pygame.display.update()
 
     def game_loop(self):
@@ -56,6 +97,9 @@ class Game:
                         player_direction_y = 0
                     elif event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                         player_direction_x = 0
-            self.player.move(player_direction_y, player_direction_x)
+            self.move_objects(player_direction_y, player_direction_x)
             self.draw_objects()
+
+            if self.check_if_collided():
+                self.set_map()
             self.clock.tick(60)
--- crossgame/player.py
+++ crossgame/player.py
@@ -7,6 +7,10 @@ class Player(GameObjects):
         
         self.speed = speed
 
-    def move(self, directiony, directionx):
+    def move(self, directiony, directionx, max_height, max_width):
+        if (self.y >= max_height - self.height and directiony > 0) or (self.y == 0 and directiony < 0):
+            return
         self.y += (directiony * self.speed)
+        if (self.x >= max_width - self.width and directionx > 0) or (self.x == 0 and directionx < 0):
+            return
         self.x += (directionx * self.speed)
